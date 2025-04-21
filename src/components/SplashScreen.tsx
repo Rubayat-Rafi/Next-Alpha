160d2dd66e0e1948ab1624d00dd0cab5cb1451a4
rubayat-rafi <rubayatrafi522@gmail.com>
2025-04-21 19:52:29 +0600

    logo size fix

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -5,10 +5,11 @@ const SplashScreen = () => {
     <div style={styles.container} className="splash-screen">
       <div className="logo-wrapper">
         <Image
-          src="/logo Main.png" 
+          src="/logo Main.png"
           alt="Alpha Video Editing Logo"
           width={150}
           height={150}
+          className="w-[80px] md:w-[120px] lg:w-[150px]"
           priority
         />
       </div>
